34c5909ca42fc19ad1fe713314053bd1a12dcf47
Lilian <lil10hand@gmail.com>
2018-10-19 18:38:19 +0200

    création des pages espace client et documents clients modification du css en suivant, modification dde documents candidats

---
+++ espace_client.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<metacharset="utf-8"/>
+     	<meta name="viewport" content="width=device-width, initial-scale=1">
+      	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+     	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+     	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+      	<link href="https://fonts.googleapis.com/css?family=Lato|Roboto" rel="stylesheet">
+      	<link rel="stylesheet" href="asset/hover.css"/>
+      	<link rel="stylesheet" href="asset/istyle.css"/>
+	</head>
+	<body>
+		<fieldset>
+			<div>
+				<label>
+					Bienvenue "..."
+				</label>
+			</div>
+			<div>
+				<input type="button" value="Déposer mes annonces" name="depot"/>
+				<input type="button" value="Gerer mes annonces" name="gestion"/>
+				<input type="button" value="Mes Documents" name="documents"/>
+			</div>
+		</fieldset>
+		<div class="-container-fluid" id="blocksearchbar">
+         <!-- Search Bar -->
+        	<div class="col-md-8 col-sm-12 col-xs-12" id="searchbar">
+            	<form class="col-md-6 navbar-form2" role="search">
+               		<div class="input-group add-on">
+                  		<input class="form-control inputsearch" placeholder="Recherche" name="srch-term" id="srch-term" type="text">
+               		</div>
+      		
+                	<button class="btn btn-default" id="boutonRecherche2" type="submit">
+                		<i class="glyphicon glyphicon-arrow-right accueil"></i>
+                	</button>
+                
+            	</form>
+            </div>
+        </div>
+	</body>
+</html>
