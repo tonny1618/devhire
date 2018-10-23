74ba5f59113989b386adeed2d677f07ec02d9311
Lilian <lil10hand@gmail.com>
2018-10-23 15:33:16 +0200

    login

--- login.php
+++ login.php
@@ -1,18 +1,18 @@
 <?php require ("head.php") ?>
 <?php require("navbar.php")?>
 
-<div class="col-sm-4 col-sm-offset-4 login-box">
+<div class="col-md-5 col-md-offset-5 login-box">
 	<div class="row">
 		<a data-toggle="tab" href="Sign In"><input class="bbtn" type="button" name="" value="Sign In"></a>
 		<a data-toggle="tab" href="Sign Up"><input class="bbtn" type="button" name="" value="Sign Up"></a>
 		<div class="textbox">
 			<i class="fas fa-user"></i>
-			<input type="text" placeholder="user" name="" value="">
+			<input type="text" placeholder="Adresse mail" name="" value="">
 		</div>
 
 		<div class="textbox">
 			<i class="fas fa-key"></i>
-			<input type="password" placeholder="password" name="" value="">
+			<input type="password" placeholder="Mot de Passe" name="" value="">
 		</div>
 		Veuillez entrez vos identifiants et mots de passe.		
 		<br/>
