c681ff35cf1c5397f357e8e658caa0278aa8203a
Jeremy <jeremy.bernard898@gmail.com>
2018-10-24 14:15:26 +0200

    page créer annonce

--- login.php
+++ login.php
@@ -1,23 +1,26 @@
 <?php require ("head.php") ?>
 <?php require("navbar.php")?>
 
-<div class="col-md-5 col-md-offset-5 login-box">
-	<div class="row">
-		<a data-toggle="tab" href="Sign In"><input class="bbtn" type="button" name="" value="Sign In"></a>
-		<a data-toggle="tab" href="Sign Up"><input class="bbtn" type="button" name="" value="Sign Up"></a>
-		<div class="textbox">
-			<i class="fas fa-user"></i>
-			<input type="text" placeholder="Adresse mail" name="" value="">
-		</div>
+<div id="login">
 
-		<div class="textbox">
-			<i class="fas fa-key"></i>
-			<input type="password" placeholder="Mot de Passe" name="" value="">
+	<div class="col-md-5 col-md-offset-5 login-box">
+		<div class="row">
+			<a data-toggle="tab" href="Sign In"><input class="bbtn" type="button" name="" value="Sign In"></a>
+			<a data-toggle="tab" href="Sign Up"><input class="bbtn" type="button" name="" value="Sign Up"></a>
+			<div class="textbox">
+				<i class="fas fa-user"></i>
+				<input type="text" placeholder="Adresse mail" name="" value="">
+			</div>
+
+			<div class="textbox">
+				<i class="fas fa-key"></i>
+				<input type="password" placeholder="Mot de Passe" name="" value="">
+			</div>
+			Veuillez entrez vos identifiants et mots de passe.		
+			<br/>
+			<a href="oubliMDP" title="oubliMDP"> Mot de passe oublié ?</a>
+			<input class="btn" type="button" name="" value="Sign In">
 		</div>
-		Veuillez entrez vos identifiants et mots de passe.		
-		<br/>
-		<a href="oubliMDP" title="oubliMDP"> Mot de passe oublié ?</a>
-		<input class="btn" type="button" name="" value="Sign In">
 	</div>
 </div>
 </body>
